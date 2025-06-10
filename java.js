29d149cce08c23586f3bb0814360ff4170145ccc
isabellablanco <ishi0001@stud.kea.dk>
2025-06-10 17:13:53 +0200

    ÆNDRING

--- java.js
+++ java.js
@@ -12,12 +12,3 @@ document.querySelectorAll('.projekt-boks').forEach(boks => {
    
   });
 
-  document.querySelectorAll('.button').forEach(button => {
-    button.addEventListener('mouseenter', () => {
-      button.style.transform = 'scale(1.05)';
-    });
-  
-    button.addEventListener('mouseleave', () => {
-      button.style.transform = 'scale(1)';
-    });
-  });
